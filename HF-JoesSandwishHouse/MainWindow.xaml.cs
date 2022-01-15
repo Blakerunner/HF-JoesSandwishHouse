8e908f6a819fe4bfdeb106bde007a43a45cec42c
Blake Michalzik <bmichalzik@gmail.com>
2022-01-15 15:13:56 -0800

    Add project files.

---
+++ HF-JoesSandwishHouse/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HF_JoesSandwishHouse
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MakeTheMenu();
+        }
+
+        private void MakeTheMenu()
+        {
+            MenuItem[] menuItems = new MenuItem[6];
+            for (int i = 0; i < menuItems.Length; i++)
+            {
+                menuItems[i] = new MenuItem();
+                if (i >= 3)
+                {
+                    menuItems[i].Breads = new string[] { "bagel", "day-old bread", "pumpernickel" };
+                }
+                menuItems[i].Generate();
+            }
+
+            item1.Text = menuItems[0].Description;
+            item2.Text = menuItems[1].Description;
+            item3.Text = menuItems[2].Description;
+            item4.Text = menuItems[3].Description;
+            item5.Text = menuItems[4].Description;
+            item6.Text = menuItems[5].Description;
+
+            price1.Text = menuItems[0].Price;
+            price2.Text = menuItems[1].Price;
+            price3.Text = menuItems[2].Price;
+            price4.Text = menuItems[3].Price;
+            price5.Text = menuItems[4].Price;
+            price6.Text = menuItems[5].Price;
+
+            MenuItem guacamoleMenuItem = new MenuItem();
+            guacamoleMenuItem.Generate();
+            guacamole.Text = $"Add guacamole for {guacamoleMenuItem.Price}";
+        }
+
+        private void newMenu_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button newMenu)
+            {
+                MakeTheMenu();
+            }
+        }
+    }
+}
---
+++ HF-JoesSandwishHouse/MenuItem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HF_JoesSandwishHouse
+{
+    internal class MenuItem
+    {
+        public static Random Randomizer = new Random();
+        public string[] Protiens = { "Roast beef", "Salami", "Ham", "Tofu", "Vegan Chicken", "Turkey" };
+        public string[] Condiments = { "mustard", "mayo", "relish", "frensh dressing", "hot sauce" };
+        public string[] Breads = { "rye", "white", "wheat", "gluten-free" };
+        public string Description;
+        public string Price;
+
+        public void Generate()
+        {
+            string randomProtein = Protiens[Randomizer.Next(Protiens.Length)];
+            string randomCondiment = Condiments[Randomizer.Next(Condiments.Length)];
+            string randomBread = Breads[Randomizer.Next(Breads.Length)];
+            Description = $"{randomProtein} with {randomCondiment} on {randomBread}";
+
+            decimal dollar = Randomizer.Next(2, 15);
+            decimal cents = Randomizer.Next(100);
+            decimal price = dollar + (cents * .01M);
+            Price = price.ToString("c");
+        }
+    }
+}
